bb4f5ed90fb193875a93abf687cf4d1d0ea3a9e6
Amine Salmi <92025603+AmineSalmi01@users.noreply.github.com>
2022-11-22 16:20:23 +0100

    .

---
+++ Prototype-basic/app/Http/Controllers/Todos_controller.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Todos;
+use Illuminate\Http\Request;
+
+class Todos_controller extends Controller
+{
+    public function index(){
+        $todosList = Todos::all();
+        return response()->json($todosList);
+    }
+
+    public function add(Request $request){
+
+        $todosList = new Todos();
+        $todosList->name = $request->name;
+        $todosList->id = $request->id;
+        $res = $todosList->save();
+
+        if($res){
+            return ["result"=>"data has been seved"];
+        }
+        else{
+            return ["result"=>"Error"];
+        }
+    }
+
+    public function show($id){
+
+        $todo = Todos::find($id);
+        // return $todo;
+        return response()->json($todo);
+
+    }
+
+    public function update(Request $request, $id){
+
+        $update = Todos::find($id);
+        $update->name = $request->name;
+    //     $update->id = $request->id;
+    //     $res = $update->save();
+
+        // if($res){
+        //     return ["result"=>"data has been adeted"];
+        // }
+        // else{
+        //     return ["result"=>"Error"];
+        // }
+        $update->save();
+
+    }
+
+    public function destroy($id){
+        $delete_todo = Todos::find($id);
+        $delete_todo->delete();
+    }
+
+
+}
