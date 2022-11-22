f7af3a869bd074e12653a6f57733ea8ade734cd0
Amine Salmi <92025603+AmineSalmi01@users.noreply.github.com>
2022-11-22 23:58:56 +0100

    .

---
+++ Prototype-standard/todolist-frontend/src/Todo.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Todo() {
+    return (
+
+        <div>
+            <h1>Todo List</h1>
+            <input/>
+            <button>Add</button>
+        </div>
+
+    );
+
+}
+export default Todo;
